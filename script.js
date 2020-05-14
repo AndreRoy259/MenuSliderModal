22278f8602a2c5506ad5a28f3339603988c14395
André Roy <roya0088@uqar.ca>
2020-05-13 22:03:08 -0400

    [ADDED] Menu slider and modal

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //! QUERY SELECTORS
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
 
 //! GLOBAL VARIABLES
 
 //! FUNCTIONS
 
 //! EVENT LISTENERS
+//* Toggle nav
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
+//* Show modal
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+//* Hode modal
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
+//* Hide modal on outside click
+window.addEventListener("click", (e) =>
+  e.target == modal ? modal.classList.remove("show-modal") : false
+);
 
 //! EXECUTION
